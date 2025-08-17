c5d7746c47364b3ae0dc086d420ca52f2cd84f25
doutorleandromendes <doutorleandromendes@gmail.com>
2025-08-17 17:11:58 -0300

    Add files via upload

--- estoque/sw.js
+++ estoque/sw.js
@@ -1,10 +1,10 @@
 
-const CACHE = 'estoque-app-v3';
-const ASSETS = ['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
-self.addEventListener('install', e => { e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))); self.skipWaiting(); });
-self.addEventListener('activate', e => { e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))); self.clients.claim(); });
-self.addEventListener('fetch', e => {
-  const url = new URL(e.request.url);
-  if (url.origin !== location.origin) { e.respondWith(fetch(e.request).catch(()=>caches.match(e.request))); return; }
-  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+var CACHE = 'estoque-app-v4';
+var ASSETS = ['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
+self.addEventListener('install', function(e){ e.waitUntil(caches.open(CACHE).then(function(c){ return c.addAll(ASSETS); })); self.skipWaiting(); });
+self.addEventListener('activate', function(e){ e.waitUntil(caches.keys().then(function(keys){ return Promise.all(keys.filter(function(k){return k!==CACHE;}).map(function(k){return caches.delete(k);})); })); self.clients.claim(); });
+self.addEventListener('fetch', function(e){
+  var url = new URL(e.request.url);
+  if (url.origin !== location.origin) { e.respondWith(fetch(e.request)['catch'](function(){ return caches.match(e.request); })); return; }
+  e.respondWith(caches.match(e.request).then(function(r){ return r || fetch(e.request); }));
 });

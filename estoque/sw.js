2aaeedd81865d127d846159f7618a489cd695302
doutorleandromendes <doutorleandromendes@gmail.com>
2025-08-17 16:57:28 -0300

    Add files via upload

--- estoque/sw.js
+++ estoque/sw.js
@@ -1,11 +1,10 @@
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open('estoque-v1').then(cache => cache.addAll([
-    './index.html',
-    './manifest.webmanifest',
-    './icon-192.png',
-    './icon-512.png'
-  ])));
-});
+
+const CACHE = 'estoque-app-v2';
+const ASSETS = ['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
+self.addEventListener('install', e => { e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))); self.skipWaiting(); });
+self.addEventListener('activate', e => { e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))); self.clients.claim(); });
 self.addEventListener('fetch', e => {
+  const url = new URL(e.request.url);
+  if (url.origin !== location.origin) { e.respondWith(fetch(e.request).catch(()=>caches.match(e.request))); return; }
   e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
 });

1f514732971510c03c309a47d3a63d3e25df21ef
doutorleandromendes <doutorleandromendes@gmail.com>
2026-06-18 17:36:40 -0300

    Add healthcheck routes and schedule initialization

--- aula-tracker/atb-routes.js
+++ aula-tracker/atb-routes.js
@@ -19,6 +19,7 @@ import { registerParecerImagemRoutes } from './atb-parecer-imagem-routes.js';
 import { ensureRetroSchema, registerFichaRetroRoutes } from './atb-ficha-retro-routes.js';
 import { ensureAdesaoSchema, registerAdesaoRoutes } from './atb-adesao-routes.js';
 import { registerConsultaRoutes } from './atb-consulta-routes.js';
+import { ensureHealthcheckTable, startHealthcheckSchedule, registerHealthcheckRoutes } from './atb-healthcheck.js';
 import { registerScihAcessoRoutes, ensureScihAcessoSchema } from './atb-scih-acesso-routes.js';
 import { ensureMirrorSchema, espelharNovaFicha } from './atb-jotform-mirror.js';
 import { ensureTriagemRegrasSchema, aplicarRegras } from './atb-triagem-regras.js';
@@ -76,6 +77,8 @@ export function registerAtbRoutes(app, pool, adminRequired, renderShell, gridReq
   registerFichaRetroRoutes(app, pool, adminRequired);
   registerAdesaoRoutes(app, pool, adminRequired);
   registerConsultaRoutes(app, pool);
+  ensureHealthcheckTable(pool).then(() => startHealthcheckSchedule(pool)).catch(e => console.error('[atb] healthcheck:', e.message));
+  registerHealthcheckRoutes(app, pool, adminRequired);
   registerScihAcessoRoutes(app, pool, adminRequired);
   registerRegrasRoutes(app, pool, adminRequired);
   registerRegrasFormRoutes(app, pool, adminRequired);
